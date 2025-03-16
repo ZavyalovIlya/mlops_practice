b2412dbbfd64078eeede94b3d5a31e8a78af2887
ZavyalovIlya <136432841+ZavyalovIlya@users.noreply.github.com>
2025-03-16 18:08:27 +0500

    Delete lab1/scripts directory

--- lab1/scripts/func.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-def pkl_dump(path, model):
-    with open(path, 'wb') as file:
-        pickle.dump(model, file)
-
-def pkl_read(path):
-    with open(path, 'rb') as file:
-        return pickle.load(file)
